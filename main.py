8d0f41c882b80e1e344e1f7b7d170ed76cfa8424
Max Carlson <max.carlson@brainium.com>
2023-04-19 14:29:33 -0700

    Graphing updates.

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ output1 = [[0], [1], [1], [1]]
 output2 = [[0], [0], [1], [0]]
 output3 = [[0], [1], [0], [1]]
 
+#output1 =  [[1,0],[0,1],[0,1],[0,1]]
+#output2 =  [[1,0],[1,0],[0,1],[1,0]]
+#output3 =  [[1,0],[0,1],[1,0],[0,1]]
+
 x = torch.Tensor(inputVar)
 y1 = torch.Tensor(output1)
 
+batchSize = 2
 dataset1 = TensorDataset(x, y1)
-dataLoader1 = DataLoader(dataset1, batch_size=4, shuffle=True)
-
-
+dataLoader1 = DataLoader(dataset1, batch_size=batchSize, shuffle=False)
 
 class NN(nn.Module):
     def __init__(self):
@@ -38,10 +41,32 @@ device = ("cpu")
 model1 = NN().to(device)
 print(model1)
 
+def graphOutputs(model, training=False):
+    model.eval()
+    inputs = np.mgrid[0:1:10j, 0:1:10j]
+    #print(inputs)
+    outputs = np.zeros((10,10))
+    #print(outputs)
+
+    for x in range(10):
+        for y in range(10):
+            out = model(torch.Tensor([inputs[0,x,y], inputs[1,x,y]])).item()
+            outputs[x,y] = out
+
+    #print(outputs)
+    plt.imshow(outputs, cmap='hot', interpolation='nearest', origin='lower')
+    plt.show()
+    if training:
+        model.train()
+
 def train(epochs, dataloader, model, lossFN, optimizer):
     size = len(dataloader.dataset)
     model.train()
+    printInterval = int(epochs / 10)
+
     for e in range(epochs):
+        if e % printInterval == 0:
+            graphOutputs(model, training=True)
         for batch, (X, y) in enumerate(dataloader):
             X, y = X.to(device), y.to(device)
 
@@ -58,11 +83,6 @@ def train(epochs, dataloader, model, lossFN, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-lossFN = torch.nn.MSELoss()
-optim = torch.optim.SGD(model1.parameters(), lr=0.01)
-train(1, dataLoader1, model1, lossFN, optim)
-
-
 def test(dataloader, model, lossFN):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -73,25 +93,16 @@ def test(dataloader, model, lossFN):
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += lossFN(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    #correct /= size
+    #print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    graphOutputs(model)    
 
 
-    inputs = np.mgrid[0:1:5j, 0:1:5j]
-    print(inputs)
-    outputs = np.zeros((5,5))
-    print(outputs)
+lossFN = torch.nn.MSELoss()
+optim = torch.optim.SGD(model1.parameters(), lr=0.5)
 
-    for x in range(5):
-        for y in range(5):
-            out = model(torch.Tensor([inputs[0,x,y], inputs[1,x,y]])).item()
-            outputs[x,y] = out
-
-    print(outputs)
-    plt.imshow(outputs, cmap='hot', interpolation='nearest', origin='lower')
-    plt.show()
-    
-
+train(15, dataLoader1, model1, lossFN, optim)
 test(dataLoader1, model1, lossFN)
