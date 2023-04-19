1f3e36dc2cbc300bb2a6b822a0fcb390a8e76377
Max Carlson <max.carlson@brainium.com>
2023-04-19 16:03:40 -0700

    Trying to get XOR to solve.

--- main.py
+++ main.py
@@ -5,71 +5,14 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
-
-inputVar = [[0,0],[0,1],[1,1],[0,1]]
-output1 = [[0], [1], [1], [1]]
-output2 = [[0], [0], [1], [0]]
-output3 = [[0], [1], [0], [1]]
+from grapher import Grapher
+from NeuralNetworks import OneLayerNN, TwoLayerNN
 
 #output1 =  [[1,0],[0,1],[0,1],[0,1]]
 #output2 =  [[1,0],[1,0],[0,1],[1,0]]
 #output3 =  [[1,0],[0,1],[1,0],[0,1]]
 
-x = torch.Tensor(inputVar)
-y1 = torch.Tensor(output1)
-
-batchSize = 2
-dataset1 = TensorDataset(x, y1)
-dataLoader1 = DataLoader(dataset1, batch_size=batchSize, shuffle=False)
-
-class NN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        #self.flatten = nn.Flatten()
-        self.model = nn.Sequential(
-            nn.Linear(2, 2),
-            nn.Sigmoid(),
-            nn.Linear(2, 1),
-            #nn.Sigmoid(),
-        )
-    def forward(self, x):
-        #x = self.flatten(x)
-        logits = self.model(x)
-        return logits
-
 device = ("cpu")
-model1 = NN().to(device)
-#print(model1)
-
-class Grapher:
-    def __init__(self):
-        self.outputs = []
-
-    def addOutput(self, model, training=False):
-        model.eval()
-        inputs = np.mgrid[0:1:10j, 0:1:10j]
-        #print(inputs)
-        outputs = np.zeros((10,10))
-        #print(outputs)
-
-        for x in range(10):
-            for y in range(10):
-                out = model(torch.Tensor([inputs[0,x,y], inputs[1,x,y]])).item()
-                outputs[x,y] = out
-
-        #print(outputs)
-        self.outputs.append(outputs)
-        if training:
-            model.train()
-
-    def graph(self):
-        cols = 4
-        rows = int(len(self.outputs) / 4)
-        fig = plt.figure(figsize=(cols, rows))
-        for i in range(len(self.outputs)):
-            fig.add_subplot(rows, cols, i+1)
-            plt.imshow(self.outputs[i], cmap='hot', interpolation='nearest', origin='lower')
-        plt.show()
 
 def train(epochs, dataloader, model, lossFN, optimizer, grapher):
     size = len(dataloader.dataset)
@@ -106,20 +49,56 @@ def test(dataloader, model, lossFN, grapher):
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += lossFN(pred, y).item()
-            #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            correct += ((pred > 0.5) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    #correct /= size
-    #print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
     grapher.addOutput(model)  
 
 
-lossFN = torch.nn.MSELoss()
-optim = torch.optim.SGD(model1.parameters(), lr=0.5)
 
-grapher = Grapher()
+def trainModel(ModelType, outputVar):
+    model = ModelType().to(device)
+    x = torch.Tensor(inputVar)
+    y1 = torch.Tensor(outputVar)
 
-train(15, dataLoader1, model1, lossFN, optim, grapher)
-test(dataLoader1, model1, lossFN, grapher)
+    lossFN = torch.nn.MSELoss()
+    #optim = torch.optim.SGD(model.parameters(), lr=1)
+    optim = torch.optim.Adam(model.parameters(), lr=0.001)
 
-grapher.graph()
+    batchSize = 4
+    epochs = 1000
+    dataset1 = TensorDataset(x, y1)
+    dataLoaderTrain = DataLoader(dataset1, batch_size=batchSize, shuffle=True)
+    dataLoaderTest = DataLoader(dataset1, batch_size=batchSize, shuffle=False)
+
+    grapher = Grapher()
+    train(epochs, dataLoaderTrain, model, lossFN, optim, grapher)
+    test(dataLoaderTest, model, lossFN, grapher)
+    grapher.graph()
+
+    model.eval()
+    t1 = model(torch.Tensor([0,0]))
+    t2 = model(torch.Tensor([0,1]))
+    t3 = model(torch.Tensor([1,1]))
+    t4 = model(torch.Tensor([1,0]))
+    ipn = 5
+
+
+
+inputVar = [[0,0],[0,1],[1,1],[0,1]]
+output1 = [[0], [1], [1], [1]] # OR
+output2 = [[0], [0], [1], [0]] # AND
+output3 = [[1], [0], [0], [0]] # ~AND
+output4 = [[1], [1], [0], [1]] # NOR
+output5 = [[0], [1], [0], [1]] # XOR
+
+
+#trainModel(OneLayerNN, output1)
+#trainModel(OneLayerNN, output2)
+#trainModel(OneLayerNN, output3)
+#trainModel(OneLayerNN, output4)
+#trainModel(OneLayerNN, output5)
+
+trainModel(TwoLayerNN, output5)
