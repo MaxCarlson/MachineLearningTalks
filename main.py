fd33b6b0b1dee9aedf00694fdc6bea5a09fa719c
Max Carlson <max.carlson@brainium.com>
2023-04-19 14:41:31 -0700

    Multiple subplots show model behavior change over time.

--- main.py
+++ main.py
@@ -39,34 +39,47 @@ class NN(nn.Module):
 
 device = ("cpu")
 model1 = NN().to(device)
-print(model1)
+#print(model1)
 
-def graphOutputs(model, training=False):
-    model.eval()
-    inputs = np.mgrid[0:1:10j, 0:1:10j]
-    #print(inputs)
-    outputs = np.zeros((10,10))
-    #print(outputs)
+class Grapher:
+    def __init__(self):
+        self.outputs = []
 
-    for x in range(10):
-        for y in range(10):
-            out = model(torch.Tensor([inputs[0,x,y], inputs[1,x,y]])).item()
-            outputs[x,y] = out
+    def addOutput(self, model, training=False):
+        model.eval()
+        inputs = np.mgrid[0:1:10j, 0:1:10j]
+        #print(inputs)
+        outputs = np.zeros((10,10))
+        #print(outputs)
 
-    #print(outputs)
-    plt.imshow(outputs, cmap='hot', interpolation='nearest', origin='lower')
-    plt.show()
-    if training:
-        model.train()
+        for x in range(10):
+            for y in range(10):
+                out = model(torch.Tensor([inputs[0,x,y], inputs[1,x,y]])).item()
+                outputs[x,y] = out
 
-def train(epochs, dataloader, model, lossFN, optimizer):
+        #print(outputs)
+        self.outputs.append(outputs)
+        if training:
+            model.train()
+
+    def graph(self):
+        cols = 4
+        rows = int(len(self.outputs) / 4)
+        fig = plt.figure(figsize=(cols, rows))
+        for i in range(len(self.outputs)):
+            fig.add_subplot(rows, cols, i+1)
+            plt.imshow(self.outputs[i], cmap='hot', interpolation='nearest', origin='lower')
+        plt.show()
+
+def train(epochs, dataloader, model, lossFN, optimizer, grapher):
     size = len(dataloader.dataset)
     model.train()
     printInterval = int(epochs / 10)
 
     for e in range(epochs):
         if e % printInterval == 0:
-            graphOutputs(model, training=True)
+            grapher.addOutput(model, training=True)
+
         for batch, (X, y) in enumerate(dataloader):
             X, y = X.to(device), y.to(device)
 
@@ -83,7 +96,7 @@ def train(epochs, dataloader, model, lossFN, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def test(dataloader, model, lossFN):
+def test(dataloader, model, lossFN, grapher):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -98,11 +111,15 @@ def test(dataloader, model, lossFN):
     #correct /= size
     #print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    graphOutputs(model)    
+    grapher.addOutput(model)  
 
 
 lossFN = torch.nn.MSELoss()
 optim = torch.optim.SGD(model1.parameters(), lr=0.5)
 
-train(15, dataLoader1, model1, lossFN, optim)
-test(dataLoader1, model1, lossFN)
+grapher = Grapher()
+
+train(15, dataLoader1, model1, lossFN, optim, grapher)
+test(dataLoader1, model1, lossFN, grapher)
+
+grapher.graph()
